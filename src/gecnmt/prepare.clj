367c29b8ce55456ab61b04dbd30eab127a8d22d9
Your Name <you@example.com>
2018-03-03 12:27:22 +0000

    Isolate

--- src/gecnmt/prepare.clj
+++ src/gecnmt/prepare.clj
@@ -1,5 +1,9 @@
 (ns gecnmt.prepare
-  (:require [gecnmt.command :as command]
+  (:require [clojure.string :as str]
+            [aid.core :as aid]
+            [cheshire.core :refer :all]
+            [me.raynes.fs :as fs]
+            [gecnmt.command :as command]
             [gecnmt.helpers :as helpers]))
 
 (def get-dataset-path
@@ -11,3 +15,35 @@
            "-o"
            (get-dataset-path "simple/extracted")
            (get-dataset-path "simple/original.xml")))
+
+(def parse-keywordize
+  (partial (aid/flip parse-string) true))
+
+(def parse-extracted
+  (comp (partial map parse-keywordize)
+        str/split-lines))
+
+(defn slurp-extracted
+  []
+  (->> #"^wiki_\d{2}"
+       (fs/find-files (get-dataset-path "simple/extracted"))
+       (map slurp)))
+
+(defn spit-parents
+  [f & more]
+  (-> f
+      fs/parent
+      fs/mkdirs)
+  (apply spit f more))
+
+(def spit-isolated
+  (aid/build spit-parents
+             (comp (partial (aid/flip str) ".txt")
+                   (partial get-dataset-path "simple/isolated")
+                   :id)
+             :text))
+
+(def isolate
+  (comp (partial run! spit-isolated)
+        (partial mapcat parse-extracted)
+        slurp-extracted))
