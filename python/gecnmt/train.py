0acad8a837f53400e4ab03a3d996a840f1602b20
Your Name <you@example.com>
2018-03-04 12:35:15 +0000

    Get glob

--- python/gecnmt/train.py
+++ python/gecnmt/train.py
@@ -1,9 +1,22 @@
+import glob
 import json
 import os.path
+
+from funcy import *
 
 
 def slurp(path):
     with open(path) as file:
         return file.read()
 
-hyperparameter = json.loads(slurp("hyperparameter/template.json"))
+
+hyperparameter = json.loads(slurp("hyperparameter/hyperparameter.json"))
+
+dataset_path = "../resources/dataset"
+
+
+def get_glob(m):
+    return os.path.join(dataset_path,
+                        m["dataset"],
+                        m["split"],
+                        "*")
