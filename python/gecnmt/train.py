7abe59ebbaab3cd0507a0ca3b5cbe11c84405733
Your Name <you@example.com>
2018-03-04 14:47:44 +0000

    Get encoder

--- python/gecnmt/train.py
+++ python/gecnmt/train.py
@@ -8,11 +8,6 @@ import torch.nn as nn
 import torch.nn.init as init
 import torchtext.vocab as vocab
 
-embedding = vocab.GloVe("6B", 50)
-vocabulary_size = first(embedding.vectors.size())
-embedding_vectors = torch.cat(
-    (embedding.vectors, init.kaiming_normal(torch.zeros(1, embedding.dim))))
-
 
 def slurp(path):
     with open(path) as file:
@@ -20,6 +15,23 @@ def slurp(path):
 
 
 hyperparameter = json.loads(slurp("hyperparameter/hyperparameter.json"))
+embedding = vocab.GloVe("6B", 50)
+vocabulary_size = first(embedding.vectors.size())
+embedding_vectors = torch.cat(
+    (embedding.vectors, init.kaiming_normal(torch.zeros(1, embedding.dim))))
+
+bag_size = 128
+
+
+def get_encoder(m):
+    model = nn.Module()
+    model.gru = nn.GRU(bag_size,
+                       m["hidden_size"],
+                       m["num_layers"],
+                       bidirectional=True,
+                       dropout=m["dropout"])
+    return model
+
 
 dataset_path = "../resources/dataset"
 
