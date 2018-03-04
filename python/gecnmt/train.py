ab649212901269538588241d5b64568fb32846fb
Your Name <you@example.com>
2018-03-04 14:21:13 +0000

    Define embedding_vectors

--- python/gecnmt/train.py
+++ python/gecnmt/train.py
@@ -3,6 +3,15 @@ import json
 import os.path
 
 from funcy import *
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+import torchtext.vocab as vocab
+
+embedding = vocab.GloVe("6B", 50)
+vocabulary_size = first(embedding.vectors.size())
+embedding_vectors = torch.cat(
+    (embedding.vectors, init.kaiming_normal(torch.zeros(1, embedding.dim))))
 
 
 def slurp(path):
