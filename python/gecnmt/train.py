41191c2e044cb7a234e2adb3ad888398dd303b71
Your Name <you@example.com>
2018-03-07 14:15:53 +0000

    Concatenate decoder_embedding and attention applied

--- python/gecnmt/train.py
+++ python/gecnmt/train.py
@@ -494,17 +494,19 @@ def pad_embedding(m):
 def decode_token(reduction, element):
     decoder_embedding = reduction["model"].embedding(
         element["input_reference_bpe"]).unsqueeze(0)
-    torch.bmm(
-        batch_transpose(
-            F.softmax(
-                reduction["model"].attention(
-                    torch.cat((decoder_embedding,
-                               get_hidden(set_val_("encoder",
-                                                   False,
-                                                   reduction))),
-                              2)),
-                2)),
-        batch_transpose(reduction["padded_embedding"]))
+    torch.cat((decoder_embedding,
+               batch_transpose(torch.bmm(
+                   batch_transpose(
+                       F.softmax(
+                           reduction["model"].attention(
+                               torch.cat((decoder_embedding,
+                                          get_hidden(set_val_("encoder",
+                                                              False,
+                                                              reduction))),
+                                         2)),
+                           2)),
+                   batch_transpose(reduction["padded_embedding"])))),
+              2)
     return reduction
 
 
