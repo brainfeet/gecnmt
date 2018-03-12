563415b57ea07e761fb0117a9a8ea1de3815f406
Your Name <you@example.com>
2018-03-12 13:20:00 +0000

    Don't include hyperparameter for rsync

--- src/gecnmt/rsync.clj
+++ src/gecnmt/rsync.clj
@@ -4,8 +4,6 @@
 (defn rsync
   [{target :target}]
   (command/rsync "-azP"
-                 "--include=resources/hyperparameter/*"
-                 "--include=python/hyperparameter/*"
                  "--exclude=.idea"
                  ;.gitignore in the project root seems to be used as a filter by default
                  "--filter=':- /python/.gitignore'"
