d1e34be915bd0d61d33e94e3345c9e53e18ea4f7
Your Name <you@example.com>
2018-03-15 23:08:43 +0000

    Call tanh

--- python/gecnmt/core.py
+++ python/gecnmt/core.py
@@ -558,21 +558,22 @@ def decode_token(reduction, element):
     decoder_embedding = torch.unsqueeze(reduction["model"].embedding(input_bpe),
                                         0)
     _, hidden = reduction["model"].decoder_gru(decoder_embedding)
-    reduction["model"].context(
-        torch.cat(
-            (batch_second_bmm(
-                F.log_softmax(
-                    batch_second_bmm(
-                        hidden,
-                        torch.transpose(
-                            reduction["model"].general(
-                                reduction["encoder_embedding"]),
-                            0,
-                            2)),
-                    2),
-                reduction["encoder_embedding"]),
-             hidden),
-            2))
+    F.tanh(
+        reduction["model"].context(
+            torch.cat(
+                (batch_second_bmm(
+                    F.log_softmax(
+                        batch_second_bmm(
+                            hidden,
+                            torch.transpose(
+                                reduction["model"].general(
+                                    reduction["encoder_embedding"]),
+                                0,
+                                2)),
+                        2),
+                    reduction["encoder_embedding"]),
+                 hidden),
+                2)))
     return reduction
 
 
