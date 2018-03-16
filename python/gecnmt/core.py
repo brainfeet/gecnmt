ab5053ee8a44e92ae95df4681213016b453907c5
Your Name <you@example.com>
2018-03-16 00:43:33 +0000

    Add a newline

--- python/gecnmt/core.py
+++ python/gecnmt/core.py
@@ -564,21 +564,22 @@ def decode_token(reduction, element):
     log_softmax_output = F.log_softmax(
         F.tanh(
             reduction["model"].context(
-                torch.cat((torch.squeeze(
-                    batch_second_bmm(
-                        F.log_softmax(
-                            batch_second_bmm(
-                                hidden,
-                                torch.transpose(
-                                    reduction["model"].general(
-                                        reduction["encoder_embedding"]),
-                                    0,
-                                    2)),
-                            2),
-                        reduction["encoder_embedding"]),
-                    0),
-                           torch.squeeze(hidden,
-                                         0)),
+                torch.cat(
+                    (torch.squeeze(
+                        batch_second_bmm(
+                            F.log_softmax(
+                                batch_second_bmm(
+                                    hidden,
+                                    torch.transpose(
+                                        reduction["model"].general(
+                                            reduction["encoder_embedding"]),
+                                        0,
+                                        2)),
+                                2),
+                            reduction["encoder_embedding"]),
+                        0),
+                     torch.squeeze(hidden,
+                                   0)),
                     1))),
         1)
     decoder_bpe = torch.squeeze(second(torch.topk(log_softmax_output, 1)), 1)
